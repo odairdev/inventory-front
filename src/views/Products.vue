c3f5cea6fa72b1c29683f630024cecb4696fd6c7
unknown <odair.dev@gmail.com>
2023-04-14 11:22:42 -0300

    Products page fully implemented and working

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -7,34 +7,45 @@
             <v-card-title class="white--text">Adicionar Produto</v-card-title>
             <v-card>
               <v-container fill-height>
-                <v-text-field
-                  label="Nome do Produto"
-                  type="text"
-                  class="px-2"
-                  v-model="name"
-                ></v-text-field>
-                <v-text-field
-                  label="Categoria"
-                  type="text"
-                  class="px-2"
-                  v-model="category"
-                ></v-text-field>
-                <v-text-field
-                  label="Quantidade"
-                  type="number"
-                  class="px-2"
-                  v-model.number="amount"
-                  min="0"
-                  max="999"
-                  :error="amount < 0"
-                ></v-text-field>
-                <v-btn
-                  color="blue accent-2"
-                  class="white--text"
-                  @click="addProduct"
-                  :disabled="invalidInputValues"
-                  >Adicionar</v-btn
-                >
+                <v-col cols="12" sm="12" md="6" lg="3">
+                  <v-text-field
+                    label="Nome do Produto"
+                    type="text"
+                    class="px-2"
+                    v-model="product.name"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="12" md="6" lg="3">
+                  <v-text-field
+                    label="Categoria"
+                    type="text"
+                    class="px-2"
+                    v-model="product.category"
+                  ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" sm="12" md="6" lg="4">
+                  <v-text-field
+                    label="Quantidade"
+                    type="number"
+                    class="px-2"
+                    v-model.number="product.amount"
+                    min="0"
+                    max="999"
+                    :error="product.amount < 0"
+                  ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" sm="12" md="6" lg="2">
+                  <v-btn
+                    min-width="100%"
+                    color="blue accent-2"
+                    class="white--text"
+                    @click="addProduct"
+                    :disabled="invalidInputValues"
+                    >Adicionar</v-btn
+                  >
+                </v-col>
               </v-container>
             </v-card>
           </v-card>
@@ -51,16 +62,71 @@
             <template v-slot:top>
               <v-text-field
                 v-model="search"
-                label="Search (UPPER CASE ONLY)"
+                label="Pesquisar"
                 class="mx-4"
               ></v-text-field>
+              <v-dialog
+                v-model="dialog"
+                max-width="500px"
+                transition="dialog-bottom-transition"
+              >
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Editar Produto</span>
+                  </v-card-title>
 
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="editedProduct.name"
+                            label="Nome do Produto"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model="editedProduct.category"
+                            label="Categoria"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            v-model.number="editedProduct.amount"
+                            label="Quantidade"
+                            type="number"
+                            min="0"
+                            max="999"
+                            :error="editedProduct.amount < 0"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialog = false">
+                      Cancelar
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="saveEdit"
+                      :disabled="invalidEditValues"
+                    >
+                      Salvar
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </template>
+
             <template v-slot:[`item.actions`]="{ item }">
-              <v-icon small class="mr-2" @click="editItem(item)">
+              <v-icon small class="mr-2" @click="editProduct(item)">
                 mdi-pencil
               </v-icon>
-              <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+              <v-icon small @click="deleteProduct(item)"> mdi-delete </v-icon>
             </template>
             <template v-slot:no-data>
               <v-btn color="primary" @click="initialize"> Reset </v-btn>
@@ -75,15 +141,22 @@
 <script>
 export default {
   async created() {
-    this.initialize()
+    this.initialize();
   },
   data() {
     return {
       dialog: false,
-      dialogDelete: false, //TRansformar em objeto
-      name: "",
-      category: "",
-      amount: null,
+      product: {
+        name: "",
+        category: "",
+        amount: null,
+      },
+      editedProduct: {
+        id: '',
+        name: "",
+        category: "",
+        amount: null,
+      },
       products: [],
       search: "",
     };
@@ -114,36 +187,69 @@ export default {
     },
     invalidInputValues() {
       return (
-        this.name.length <= 0 ||
-        this.category.length <= 0 ||
-        this.amount <= 0 ||
-        !Number.isInteger(this.amount)
+        this.product.name.length <= 0 ||
+        this.product.category.length <= 0 ||
+        this.product.amount <= 0 ||
+        !Number.isInteger(this.product.amount)
+      )
+    },
+    invalidEditValues() {
+      return (
+        this.editedProduct.name.length <= 0 ||
+        this.editedProduct.category.length <= 0 ||
+        this.editedProduct.amount <= 0 ||
+        !Number.isInteger(this.editedProduct.amount)
       );
     },
   },
   methods: {
     async addProduct() {
-      const newProduct = {
-        name: this.name,
-        category: this.category,
-        amount: this.amount,
-      };
-
       try {
-        await this.$http.post("products", newProduct);
+        await this.$http.post("products", this.product);
         alert("Produto cadastrado com sucesso.");
-        this.initialize()
+        this.initialize();
       } catch (err) {
         console.log(err);
+      }
+    },
+    editProduct(product) {
+      this.dialog = true;
+      this.editedProduct.id = product.id;
+      this.editedProduct.name = product.name;
+      this.editedProduct.category = product.category;
+      this.editedProduct.amount = product.amount;
+    },
+    async deleteProduct(product) {
+      try {
+        if (confirm(`Deseja deletar o produto ${product.name}?`)) {
+          await this.$http.delete(`products/${product.id}`);
+          this.initialize();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    async saveEdit() {
+      try {
+        console.log(this.editedProduct)
+        await this.$http.put('products', this.editedProduct)
+        alert('Produto editado com sucesso')
+        this.dialog = false
+        this.initialize()
+      }catch(err) {
+        console.log(err)
       }
     },
     async initialize() {
       try {
         const response = await this.$http.get("products");
         this.products = response.data.products;
-        this.name = ''
-        this.category = ''
-        this.amount = null
+        this.product = {
+          name: "",
+          category: "",
+          amount: null,
+        };
       } catch (err) {
         console.log(err);
       }
