443db7af2355bdad5db22b58bd57dec84865c7ab
unknown <odair.dev@gmail.com>
2023-04-16 17:23:32 -0300

    Route transition added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <v-app app>
     <v-main>
       <Header v-if="signed" />
-      <router-view />
+      <transition name="slide" mode="out-in">
+        <router-view />
+      </transition>
     </v-main>
   </v-app>
 </template>
@@ -23,3 +25,23 @@ export default {
   },
 };
 </script>
+
+<style>
+	@keyframes slide-in {
+		from { transform: translateY(-30px); opacity: 0;}
+		to { transform: translateY(0px); opacity: 1;}
+	}
+
+	@keyframes slide-out {
+		from { transform: translateY(0px); opacity: 1;}
+		tp { transform: translateY(-30px); opacity: 0;}
+	}
+
+	.slide-enter-active {
+		animation: slide-in 0.3s ease
+	}
+
+	.slide-leave-active {
+		animation: slide-out 0.3s ease;
+	}
+</style>
